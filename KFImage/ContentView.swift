4fb4a83b4773b6a016fcfccaff05554abddf1024
Elaidzha1940 <eli080293@gmail.com>
2023-10-20 18:15:30 +0300

    Done

--- KFImage/ContentView.swift
+++ KFImage/ContentView.swift
@@ -16,11 +16,11 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-          
+            
             KFImage(URL(string: "https://images.unsplash.com/photo-1694952751871-4c60120ec470?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyMnxKcGc2S2lkbC1Ia3x8ZW58MHx8fHx8"))
                 .loadDiskFileSynchronously()
                 .cacheMemoryOnly()
-                .fade(duration: 2.5)
+                .fade(duration: 4.5)
                 .onProgress { receivedSize, totalSize in
                     progress = Int32(receivedSize)
                 }
