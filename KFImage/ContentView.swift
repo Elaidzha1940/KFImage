36cae1035399f4a4c5729a9672adcaa9a00ef08d
Elaidzha1940 <eli080293@gmail.com>
2023-10-20 18:12:10 +0300

    Updated Int32

--- KFImage/ContentView.swift
+++ KFImage/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 import Kingfisher
 
 struct ContentView: View {
+    @State private var progress: Int32 = 0
+    
     var body: some View {
         
         ZStack {
@@ -18,7 +20,10 @@ struct ContentView: View {
             KFImage(URL(string: "https://images.unsplash.com/photo-1694952751871-4c60120ec470?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyMnxKcGc2S2lkbC1Ia3x8ZW58MHx8fHx8"))
                 .loadDiskFileSynchronously()
                 .cacheMemoryOnly()
-                .fade(duration: 0.5)
+                .fade(duration: 2.5)
+                .onProgress { receivedSize, totalSize in
+                    progress = Int32(receivedSize)
+                }
             
             Rectangle()
                 .foregroundColor(.white)
@@ -27,6 +32,10 @@ struct ContentView: View {
                 .opacity(0.3)
             HStack{
                 Text("Welcome to elid.ev")
+                    .font(.system(size: 20, weight: .bold, design: .serif))
+                    .foregroundStyle(.white)
+                
+                Text("\(progress)")
                     .font(.system(size: 20, weight: .bold, design: .serif))
                     .foregroundStyle(.white)
             }
