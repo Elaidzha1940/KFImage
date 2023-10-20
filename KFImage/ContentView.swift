4c8cd38b015c841eac2690e3197553957b424fd3
Elaidzha1940 <eli080293@gmail.com>
2023-10-20 17:57:11 +0300

    KFImage + Updated URL Image

--- KFImage/ContentView.swift
+++ KFImage/ContentView.swift
@@ -15,7 +15,9 @@ struct ContentView: View {
         
         ZStack {
           
-            KFImage(URL(string: "https://images.unsplash.com/photo-1696199386921-0f8f7b03973d?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzMXxKcGc2S2lkbC1Ia3x8ZW58MHx8fHx8"))
+            KFImage(URL(string: "https://images.unsplash.com/photo-1694952751871-4c60120ec470?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyMnxKcGc2S2lkbC1Ia3x8ZW58MHx8fHx8"))
+            Rectangle()
+                .frame(width: 300, height: 200)
         }
     }
 }
